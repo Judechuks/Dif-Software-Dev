24d425de9d88c9f6d9be3c0901a2666f34eabd48
Jude Chuks <judechuks26@gmail.com>
2025-07-05 10:13:34 +0100

    react-router-dom basics

---
+++ Assignments/05-React-Router-Dom/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <main className="">
+      <h1 className="text-center">About Page</h1>
+    </main>
+  );
+};
+
+export default About;
---
+++ Assignments/05-React-Router-Dom/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="">
+      <h1 className="text-center">Home Page</h1>
+    </main>
+  );
+};
+
+export default Home;
