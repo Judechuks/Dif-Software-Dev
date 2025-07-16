46052278e08b8ce02176e8a8d066b21f80464995
Jude Chuks <judechuks26@gmail.com>
2025-07-16 10:15:04 +0100

    app setup

--- Assignments/05-React-Router-Dom/src/App.jsx
+++ Assignments/05-React-Router-Dom/src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Footer from "./sections/Footer";
+import { CartProvider } from "./context/CartContext";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
