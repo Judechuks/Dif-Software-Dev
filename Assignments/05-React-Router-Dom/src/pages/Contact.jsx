d158d7d067ef0554b7055b2c4f1ca84f0e9d2cda
Jude Chuks <judechuks26@gmail.com>
2025-07-16 10:14:38 +0100

    empty contact page

--- Assignments/05-React-Router-Dom/src/pages/Contact.jsx
+++ Assignments/05-React-Router-Dom/src/pages/Contact.jsx
@@ -1,12 +1,7 @@
-import { useContext } from "react";
-import UserContext from "../context/userContext";
-
 const Contact = () => {
-  const { user } = useContext(UserContext);
   return (
     <main className="">
       <h1 className="text-center">Contact Page</h1>
-      <h2>Contact me using: {user.phone}</h2>
     </main>
   );
 };
